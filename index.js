373344b9f3153704b379f1d56f1b057d7bb89535
Mercy <mercycherotich757@gmail.com>
2021-12-11 01:25:55 +0300

    display

--- index.js
+++ index.js
@@ -3,10 +3,17 @@ birthDate.addEventListener('click', function(e) {
     e.preventDefault();
 
     var date1 = document.querySelector("#date").value;
-    var date1 = document.querySelector("#date").value;
     var year = document.querySelector("#year").value;
     var show = document.querySelector(".show");
     var gender = $("#gender").val();
     var month = $("#month").val();
+    var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+    var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+    var days = ["Sunday", "Monday", "Tuesday", "wednesday", "Thursday", "Friday", "Saturday"];
+    var date2 = parseInt(date1);
+    var date3 = month + "" + date2 + "" + year;
+    var finalDate = new Date(date3);
 
+    const Div1 = document.createElement('div');
+    Div1.classList.add('display');
 })
