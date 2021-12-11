ba65c9c106d433dfa1d64e1c560c05f09868daac
Mercy <mercycherotich757@gmail.com>
2021-12-11 13:44:25 +0300

    add if loop that returns male name according to day born

--- index.js
+++ index.js
@@ -18,9 +18,26 @@ birthDate.addEventListener('click', function(e) {
     Div1.classList.add('display');
 
     let dateOfBirth = finalDate.getDay();
-    if (gender == "Male") {
-        if (dateOfBirth == 0)
-
+    if (gender == "Male" && dateOfBirth == 0) {
+        todoItem.innerHTML = "Your Akan Ghanian name is " + maleNames[0] + ". you were born on " + days[0];
+    }
+    if (gender == "Male" && dateOfBirth == 1) {
+        todoItem.innerHTML = "Your Akan Ghanian name is " + maleNames[1] + ". you were born on " + days[1];
+    }
+    if (gender == "Male" && dateOfBirth == 2) {
+        todoItem.innerHTML = "Your Akan Ghanian name is " + maleNames[2] + ". you were born on " + days[2];
+    }
+    if (gender == "Male" && dateOfBirth == 3) {
+        todoItem.innerHTML = "Your Akan Ghanian name is " + maleNames[3] + ". you were born on " + days[3];
+    }
+    if (gender == "Male" && dateOfBirth == 4) {
+        todoItem.innerHTML = "Your Akan Ghanian name is " + maleNames[4] + ". you were born on " + days[4];
+    }
+    if (gender == "Male" && dateOfBirth == 5) {
+        todoItem.innerHTML = "Your Akan Ghanian name is " + maleNames[5] + ". you were born on " + days[5];
+    }
+    if (gender == "Male" && dateOfBirth == 6) {
+        todoItem.innerHTML = "Your Akan Ghanian name is " + maleNames[6] + ". you were born on " + days[6];
     }
 
 })
