fbcced5115aea59a2fe76f8125f96a495c0ef4fa
Mercy <mercycherotich757@gmail.com>
2021-12-11 19:27:37 +0300

    add BDD to README

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # The_Burger_Palace
 https://github.com/macc254/Birthday-generator.git
-The_Burger_Palace
+Birthday-generator
 # Author
 Mercy Bore.
 ### Description
-The website entails more about a restaurant selling burgers.
+This is a simple website where you key in your birthday, it generates the day of the week you were born. It uses that day to return an Akan name depending on whether you choose your gender as male or female.
 ### Prerequisities
 You need to have git installed
 You can install it with the following command in your terminal
@@ -15,11 +15,14 @@ To access this project on your local files, you can clone it using these steps
 1. Use this command to clone `$ git clone
 https://github.com/macc254/Birthday-generator.git
  This will clone the repository into your local folder.
+ #### BDD
+    First select your gender, enter the date you were born, select the month you were born , select the year you were born.Click submit the program will calculate and return a digit between 0 and 6 that represents the day of the week. Your Akan name will be assigned according to the day of the week found.
 
 #### Technologies Used
 1. HTML
-1. CSS
-1. Git
+2. JavaScript
+3. CSS
+4. Git
 ##### Live Site
 View [Live]
 https://macc254.github.io/Birthday-generator/
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var birthDate = document.querySelector("#birthDate")
 birthDate.addEventListener('click', function(e) {
     e.preventDefault();
 
-    var date1 = document.querySelector("#date").value;
+    var dob = document.querySelector("#date").value;
     var year = document.querySelector("#year").value;
     var show = document.querySelector(".show");
     var gender = $("#gender").val();
@@ -10,34 +10,62 @@ birthDate.addEventListener('click', function(e) {
     var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
     var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
     var days = ["Sunday", "Monday", "Tuesday", "wednesday", "Thursday", "Friday", "Saturday"];
-    var date2 = parseInt(date1);
+    var date2 = parseInt(dob);
     var date3 = month + "" + date2 + "" + year;
     var finalDate = new Date(date3);
 
     const Div1 = document.createElement('div');
     Div1.classList.add('display');
 
+    const Div1 = document.createElement('div');
+    Div1.classList.add('display');
+
+    const todoItem = document.createElement('li')
+    todoItem.classList.add('display')
+    Div1.appendChild(todoItem)
+    show.appendChild(Div1);
+
     let dateOfBirth = finalDate.getDay();
     if (gender == "Male" && dateOfBirth == 0) {
-        todoItem.innerHTML = "Your Akan Ghanian name is " + maleNames[0] + ". you were born on " + days[0];
+        todoItem.innerHTML = "Your Akan Ghanian name is " + maleNames[0] + ". you were born on " + days[0]
     }
     if (gender == "Male" && dateOfBirth == 1) {
-        todoItem.innerHTML = "Your Akan Ghanian name is " + maleNames[1] + ". you were born on " + days[1];
+        todoItem.innerHTML = "Your Akan Ghanian name is " + maleNames[1] + ". you were born on " + days[1]
     }
     if (gender == "Male" && dateOfBirth == 2) {
-        todoItem.innerHTML = "Your Akan Ghanian name is " + maleNames[2] + ". you were born on " + days[2];
+        todoItem.innerHTML = "Your Akan Ghanian name is " + maleNames[2] + ". you were born on " + days[2]
     }
     if (gender == "Male" && dateOfBirth == 3) {
-        todoItem.innerHTML = "Your Akan Ghanian name is " + maleNames[3] + ". you were born on " + days[3];
+        todoItem.innerHTML = "Your Akan Ghanian name is " + maleNames[3] + ". you were born on " + days[3]
     }
     if (gender == "Male" && dateOfBirth == 4) {
-        todoItem.innerHTML = "Your Akan Ghanian name is " + maleNames[4] + ". you were born on " + days[4];
+        todoItem.innerHTML = "Your Akan Ghanian name is " + maleNames[4] + ". you were born on " + days[4]
     }
     if (gender == "Male" && dateOfBirth == 5) {
-        todoItem.innerHTML = "Your Akan Ghanian name is " + maleNames[5] + ". you were born on " + days[5];
+        todoItem.innerHTML = "Your Akan Ghanian name is " + maleNames[5] + ". you were born on " + days[5]
     }
     if (gender == "Male" && dateOfBirth == 6) {
-        todoItem.innerHTML = "Your Akan Ghanian name is " + maleNames[6] + ". you were born on " + days[6];
+        todoItem.innerHTML = "Your Akan Ghanian name is " + maleNames[6] + ". you were born on " + days[6]
     }
-
+    if (gender == "Female" && dateOfBirth == 0) {
+        todoItem.innerHTML = "Your Akan Ghanian name is " + femaleNames[0] + ". you were born on " + days[0]
+    }
+    if (gender == "Female" && dateOfBirth == 1) {
+        todoItem.innerHTML = "Your Akan Ghanian name is " + femaleNames[1] + ". you were born on " + days[1]
+    }
+    if (gender == "Female" && dateOfBirth == 2) {
+        todoItem.innerHTML = "Your Akan Ghanian name is " + femaleNames[2] + ". you were born on " + days[2]
+    }
+    if (gender == "Female" && dateOfBirth == 3) {
+        todoItem.innerHTML = "Your Akan Ghanian name is " + femaleNames[3] + ". you were born on " + days[3]
+    }
+    if (gender == "Female" && dateOfBirth == 4) {
+        todoItem.innerHTML = "Your Akan Ghanian name is " + femaleNames[4] + ". you were born on " + days[4]
+    }
+    if (gender == "Female" && dateOfBirth == 5) {
+        todoItem.innerHTML = "Your Akan Ghanian name is " + femaleNames[5] + ". you were born on " + days[5]
+    }
+    if (gender == "Female" && dateOfBirth == 6) {
+        todoItem.innerHTML = "Your Akan Ghanian name is " + femaleNames[6] + ". you were born on " + days[6]
+    }
 })
